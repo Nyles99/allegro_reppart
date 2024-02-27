ab36157eb547638fc4e372de9b8d5bd667ab9a7e
Никита Молчанов <nylesszpg@gmail.com>
2024-02-27 16:22:26 +0300

    переходим к радиатором + функция нарисовалась

--- main.py
+++ main.py
@@ -71,231 +71,239 @@ for item_one_href in first_href:
 with open("json_href_str.json", "a", encoding="utf-8") as file:
     json.dump(json_href_str, file, indent=4, ensure_ascii=False)"""
 
+def osnova(name_part, all_text, href_str):
+
+    if os.path.exists(f"allegro_{name_part}_{time.strftime('%Y-%m-%d')}.csv"):
+        print("файл с таким именем уже есть")
+    else:
+        with open(f"allegro_{name_part}_{time.strftime('%Y-%m-%d')}.csv", "w", encoding="utf-8") as file_data:
+            writer = csv.writer(file_data)
+
+            writer.writerow(
+                (
+                    "АРТИКУЛ",
+                    "НОМЕР ДЕТАЛИ",
+                    "МАРКА",
+                    "МОДЕЛЬ",
+                    "ГОД",
+                    "ССЫЛКА НА ЗАПЧАСТЬ",
+                    "ТОПЛИВО",
+                    "ОБЪЕМ",
+                    "ТИП ДВИГАТЕЛЯ",
+                    "КОРОБКА",
+                    "ТИП КУЗОВА",
+                    "ЗАПЧАСТЬ",
+                    "ОПИСАНИЕ",
+                    "ПОД ЗАКАЗ",
+                    "ЦЕНА",
+                    "НОВАЯ",
+                    "ФОТО",
+                    "ФИРМА ПРОИЗВОДИТЕЛЬ",
+                    "ОРИГИНАЛ",
+                    "ВЕРСИЯ СБОРКИ",
+                    "ВЕС ДЛЯ ОТГРУЗКИ",
+                    "СТРАНИЦА ОКОНЧАНИЯ",
+                )
+            )
+
+
+
+    
+    quantity = ''
+    for char in all_text:
+        if char == "0" or char == "1" or char == "2" or char == "3" or char == "4" or char == "5" or char == "6" or char == "7" or char == "8" or char == "9":
+            quantity = quantity + char
+    page = int(int(quantity)/30 + 2)
+    #print(page)
+    count_num_zap = []
+    count = 1
+    for i in range(1, int(page)):
+        href_page = f"{href_str}?id-page={i}&id-per-page=30"
+        print(href_page)
+        req = requests.get(url=href_page, headers=headers)
+        src = req.text
+        
+        soup = BeautifulSoup(src, "lxml")
+        #print(soup)
+        cards_obj = soup.find_all("a", class_="b-item-link")
+        #print(cards_obj)
+        for item_card in cards_obj:
+            href_card = "https://wallegro.ru" + item_card.get("href")
+            #print(href_card)
+            req = requests.get(url=href_card, headers=headers)
+            src = req.text
+            
+            soup = BeautifulSoup(src, "lxml")
+            art_obj = soup.find_all("div", class_="timeline")
+            #print(art_obj)
+            artical = ""
+            for item_art in art_obj:
+                #print(item_art.text)
+                for char in item_art.text:
+                    #print(char)
+                    if char == "0" or char == "1" or char == "2" or char == "3" or char == "4" or char == "5" or char == "6" or char == "7" or char == "8" or char == "9":
+                        artical = artical + char
+            print(artical)
+            status = "б.у."
+            num_zap = " "
+            firma = " "
+            version = " "
+            original = " "
+            all_info_obj = (soup.find_all("p"))
+            weight = " "
+            #print(all_info_obj)
+            for item in all_info_obj:
+                #print(item, "туда")
+                item = str(item)
+                if "Состояние" or "состояние" in item: 
+                    if "Новый" in item:
+                        status = "Новый"
+                if "Номер детали" in item:
+                    num_zap = item[item.find('<b class="translable">')+22 : item.find('</b></small>')]
+                    list_num_zap = num_zap.split()
+                    num_zap = ''
+                    for slovo in list_num_zap:
+                        if ("0" or "1" or "2" or "3" or "4" or "5" or "6" or "7" or "8" or "9") in slovo:
+                            num_zap = num_zap + "; " + slovo
+                if "Производитель части" in item:
+                    firma = item[item.find('<b class="translable">')+22 : item.find('</b></small>')]
+                if '"translable">Версия</span>' in item:
+                    version = item[item.find('<b class="translable">')+22 : item.find('</b></small>')]
+                if '"translable">Качество деталей' in item:
+                    original = item[item.find('<b class="translable">')+22 : item.find('</b></small>')]
+                if '"translable">Вес с индивидуальной упаковкой' in item:
+                    weight = item[item.find('<b class="translable">')+22 : item.find('</b></small>')]
+            if num_zap not in count_num_zap:
+                count_num_zap.append(num_zap)  
+                print(status)
+                print(num_zap)
+                print(firma)
+                print(version)
+                print(original)
+                print(name_part)
+                print(weight)
+                print(href_card)
+                
+                
+                img_obj = soup.find_all("div", class_="container", style="padding: 0px 0 0 0")
+                #print(img_obj)
+                for item in img_obj:
+                    #print(item, "Новая строка")
+                    href_foto = str(item)
+                    #print(href_foto)
+                    href_foto = href_foto[href_foto.find('"image": [')+12 : href_foto.find('],')- 2].replace("/n","").replace("\r","").replace(" ","")
+                    href_foto =  href_foto[href_foto.find('"')+1 : href_foto.find('" ')-1]
+                print(href_foto)
+                price_obj = soup.find_all("span", itemprop="price")
+                #print(price_obj)
+                for item in price_obj:
+                    price = item.text
+                print(price)
+
+                marka_and_model_obj = (soup.find_all("h1", itemprop="name", class_="translable"))
+                #print(marka_and_model_obj)
+                for item_mm in marka_and_model_obj:
+                    info = item_mm.text.lower()
+                    spisok_marka_model = info.split()
+                print(info)
+                year = " "
+                year_obj = soup.find_all("div", class_="translable")
+                #print(year_obj)
+                for item_year in year_obj:
+                    item_year = item_year.text
+                    for word_year in year_list:
+                    
+                        if word_year in item_year:
+                        #print(word_year)
+                            year = word_year
+                
+                    
+                
+                with open('modelu_new.json', encoding="utf-8") as file:
+                    model_need_list = json.load(file)
+                
+                for model, marka in model_need_list.items():
+                    if str(marka).lower() in info:
+                        marka = str(marka).upper()
+                        break
+                string_model = " "
+                
+                for model, marka_1 in model_need_list.items():
+                    for word in spisok_marka_model:
+                        if len(word) > 1:
+                            if word.lower() in model:
+                                if word.lower() not in string_model:
+                                    string_model = str(string_model).upper() + "!!! "+ str(word).upper()
+                #print(string_model, "Смотреть сюда")
+                string_model = string_model[4 : ]
+                string_model = string_model[: string_model.find("!!! ")]                
+                print(year)       
+                print(string_model)
+                print(marka)
+                fuel = ""
+                engine = ""
+                transmission = ""
+                car_body = ""
+                volume = ""
+                order = ""
+                nomer_str = 1
+                if int(price) > 500:
+                    file = open(f"allegro_{name_part}_{time.strftime('%Y-%m-%d')}.csv", "a", encoding="utf-8", newline='')
+                    writer = csv.writer(file)
+
+                    writer.writerow(
+                        (
+                            artical,
+                            num_zap,
+                            marka,
+                            string_model,
+                            year,
+                            href_card,
+                            fuel,
+                            volume,
+                            engine,
+                            transmission,
+                            car_body,
+                            name_part,
+                            info,
+                            order,
+                            price,
+                            status,
+                            href_foto,
+                            firma,
+                            original,
+                            version,
+                            weight,
+                            nomer_str
+                        )
+                    )
+                    file.close()
+            else:
+                print(f"Уже было, давай следующую! Повторений уже {count}" )
+                count += 1
+
+
+
+
+
 with open('json_href_str.json', encoding="utf-8") as file:
     all = json.load(file)
 nomer = 2
 for all_text, href_str in all.items():
     #print(href_str)
-    if nomer == 117:
+    if nomer == 79:
         print(all_text)
         if href_str == "https://wallegro.ru/cat/260576-Sistema-ABS-i-ESP.html":
             href_str = "https://wallegro.ru/cat/260618-Drajvery-ABS.html"
             name_part = "Блок ABS"
             all_text = "Система ABS и ESP 22666"
             
-        if "Решетки радиатора" in all_text:
+        if href_str == "https://wallegro.ru/cat/254664-Reshetki-radiatora.html":
             name_part = "Решетка радиатора"
-            
+
+        if href_str == "https://wallegro.ru/cat/18690-Radiatory.html":    
         
 
-        if os.path.exists(f"allegro_{name_part}_{time.strftime('%Y-%m-%d')}.csv"):
-            print("файл с таким именем уже есть")
-        else:
-            with open(f"allegro_{name_part}_{time.strftime('%Y-%m-%d')}.csv", "w", encoding="utf-8") as file_data:
-                writer = csv.writer(file_data)
-
-                writer.writerow(
-                    (
-                        "АРТИКУЛ",
-                        "НОМЕР ДЕТАЛИ",
-                        "МАРКА",
-                        "МОДЕЛЬ",
-                        "ГОД",
-                        "ССЫЛКА НА ЗАПЧАСТЬ",
-                        "ТОПЛИВО",
-                        "ОБЪЕМ",
-                        "ТИП ДВИГАТЕЛЯ",
-                        "КОРОБКА",
-                        "ТИП КУЗОВА",
-                        "ЗАПЧАСТЬ",
-                        "ОПИСАНИЕ",
-                        "ПОД ЗАКАЗ",
-                        "ЦЕНА",
-                        "НОВАЯ",
-                        "ФОТО",
-                        "ФИРМА ПРОИЗВОДИТЕЛЬ",
-                        "ОРИГИНАЛ",
-                        "ВЕРСИЯ СБОРКИ",
-                        "ВЕС ДЛЯ ОТГРУЗКИ",
-                        "СТРАНИЦА ОКОНЧАНИЯ",
-                    )
-                )
-
-
-
-        
-        quantity = ''
-        for char in all_text:
-            if char == "0" or char == "1" or char == "2" or char == "3" or char == "4" or char == "5" or char == "6" or char == "7" or char == "8" or char == "9":
-                quantity = quantity + char
-        page = int(int(quantity)/30 + 2)
-        #print(page)
-        count_num_zap = []
-        count = 1
-        for i in range(1, int(page)):
-            href_page = f"{href_str}?id-page={i}&id-per-page=30"
-            print(href_page)
-            req = requests.get(url=href_page, headers=headers)
-            src = req.text
-            
-            soup = BeautifulSoup(src, "lxml")
-            #print(soup)
-            cards_obj = soup.find_all("a", class_="b-item-link")
-            #print(cards_obj)
-            for item_card in cards_obj:
-                href_card = "https://wallegro.ru" + item_card.get("href")
-                #print(href_card)
-                req = requests.get(url=href_card, headers=headers)
-                src = req.text
-                
-                soup = BeautifulSoup(src, "lxml")
-                art_obj = soup.find_all("div", class_="timeline")
-                #print(art_obj)
-                artical = ""
-                for item_art in art_obj:
-                    #print(item_art.text)
-                    for char in item_art.text:
-                        #print(char)
-                        if char == "0" or char == "1" or char == "2" or char == "3" or char == "4" or char == "5" or char == "6" or char == "7" or char == "8" or char == "9":
-                            artical = artical + char
-                print(artical)
-                status = "б.у."
-                num_zap = " "
-                firma = " "
-                version = " "
-                original = " "
-                all_info_obj = (soup.find_all("p"))
-                weight = " "
-                #print(all_info_obj)
-                for item in all_info_obj:
-                    #print(item, "туда")
-                    item = str(item)
-                    if "Состояние" or "состояние" in item: 
-                        if "Новый" in item:
-                            status = "Новый"
-                    if "Номер детали" in item:
-                        num_zap = item[item.find('<b class="translable">')+22 : item.find('</b></small>')]
-                        list_num_zap = num_zap.split()
-                        num_zap = ''
-                        for slovo in list_num_zap:
-                            if ("0" or "1" or "2" or "3" or "4" or "5" or "6" or "7" or "8" or "9") in slovo:
-                                num_zap = num_zap + "; " + slovo
-                    if "Производитель части" in item:
-                        firma = item[item.find('<b class="translable">')+22 : item.find('</b></small>')]
-                    if '"translable">Версия</span>' in item:
-                        version = item[item.find('<b class="translable">')+22 : item.find('</b></small>')]
-                    if '"translable">Качество деталей' in item:
-                        original = item[item.find('<b class="translable">')+22 : item.find('</b></small>')]
-                    if '"translable">Вес с индивидуальной упаковкой' in item:
-                        weight = item[item.find('<b class="translable">')+22 : item.find('</b></small>')]
-                if num_zap not in count_num_zap:
-                    count_num_zap.append(num_zap)  
-                    print(status)
-                    print(num_zap)
-                    print(firma)
-                    print(version)
-                    print(original)
-                    print(name_part)
-                    print(weight)
-                    print(href_card)
-                    
-                    
-                    img_obj = soup.find_all("div", class_="container", style="padding: 0px 0 0 0")
-                    #print(img_obj)
-                    for item in img_obj:
-                        #print(item, "Новая строка")
-                        href_foto = str(item)
-                        #print(href_foto)
-                        href_foto = href_foto[href_foto.find('"image": [')+12 : href_foto.find('],')- 2].replace("/n","").replace("\r","").replace(" ","")
-                        href_foto =  href_foto[href_foto.find('"')+1 : href_foto.find('" ')-1]
-                    print(href_foto)
-                    price_obj = soup.find_all("span", itemprop="price")
-                    #print(price_obj)
-                    for item in price_obj:
-                        price = item.text
-                    print(price)
-
-                    marka_and_model_obj = (soup.find_all("h1", itemprop="name", class_="translable"))
-                    #print(marka_and_model_obj)
-                    for item_mm in marka_and_model_obj:
-                        info = item_mm.text.lower()
-                        spisok_marka_model = info.split()
-                    print(info)
-                    year = " "
-                    year_obj = soup.find_all("div", class_="translable")
-                    #print(year_obj)
-                    for item_year in year_obj:
-                        item_year = item_year.text
-                        for word_year in year_list:
-                        
-                            if word_year in item_year:
-                            #print(word_year)
-                                year = word_year
-                    
-                        
-                    
-                    with open('modelu_new.json', encoding="utf-8") as file:
-                        model_need_list = json.load(file)
-                    
-                    for model, marka in model_need_list.items():
-                        if str(marka).lower() in info:
-                            marka = str(marka).upper()
-                            break
-                    string_model = " "
-                    
-                    for model, marka_1 in model_need_list.items():
-                        for word in spisok_marka_model:
-                            if len(word) > 1:
-                                if word.lower() in model:
-                                    if word.lower() not in string_model:
-                                        string_model = str(string_model).upper() + "!!! "+ str(word).upper()
-                    #print(string_model, "Смотреть сюда")
-                    string_model = string_model[4 : ]
-                    string_model = string_model[: string_model.find("!!! ")]                
-                    print(year)       
-                    print(string_model)
-                    print(marka)
-                    fuel = ""
-                    engine = ""
-                    transmission = ""
-                    car_body = ""
-                    volume = ""
-                    order = ""
-                    nomer_str = 1
-                    if int(price) > 500:
-                        file = open(f"allegro_{name_part}_{time.strftime('%Y-%m-%d')}.csv", "a", encoding="utf-8", newline='')
-                        writer = csv.writer(file)
-
-                        writer.writerow(
-                            (
-                                artical,
-                                num_zap,
-                                marka,
-                                string_model,
-                                year,
-                                href_card,
-                                fuel,
-                                volume,
-                                engine,
-                                transmission,
-                                car_body,
-                                name_part,
-                                info,
-                                order,
-                                price,
-                                status,
-                                href_foto,
-                                firma,
-                                original,
-                                version,
-                                weight,
-                                nomer_str
-                            )
-                        )
-                        file.close()
-                else:
-                    print(f"Уже было, давай следующую! Повторений уже {count}" )
-                    count += 1
         
     nomer += 1
     
